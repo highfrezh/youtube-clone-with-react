a51efa206b7eecf8718a56eeca393f7215cfe5e8
highfrezh <highfrezh14@gmail.com>
2025-06-24 00:26:38 +0100

    vite.config edit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/youtube-clone-with-react'
+  base: 'https://highfrezh.github.io/youtube-clone-with-react/'
 })
