df8643314e47863111f362e5233d5eb8e0671a7a
highfrezh <highfrezh14@gmail.com>
2025-06-24 00:38:29 +0100

    main.jsx edit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/youtube-clone-with-react">
       <App />
     </BrowserRouter>
   </StrictMode>,
